a08b8ae8eeafe6ba3333c72dd7b03b30fb138079
Shinya Takeuchi <bamboo@rj.dream.jp>
2015-09-05 14:05:55 +0900

    #3 For messages of srvcmd

--- src/srvcmd/srvcmd.cpp
+++ src/srvcmd/srvcmd.cpp
@@ -5,6 +5,13 @@
 #include <wtsapi32.h>
 #include "..\..\..\YaizuComLib\src\msgproc\msgproc.h"
 
+#define CMDFRK_SVCFAILEDTOSTART 3142
+#define CMDFRK_SVCFAILEDTOSTOP 3143
+#define CMDFRK_SVCALREADYSTART 3144
+#define CMDFRK_SVCALREADYSTOP 3145
+#define CMDFRK_SVCSTART 3146
+#define CMDFRK_SVCSTOP 3147
+
 BOOL DebugOption = FALSE;
 
 BOOL IsProcessStarting()
@@ -31,6 +38,30 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	char Param[256] = "";
 	sscanf_s(lpCmdLine, "%s", Param);
 
+	//3142 (NC6)
+	MessageProc::AddJpn(CMDFRK_SVCFAILEDTOSTART, _T("CmdFreakサービスの開始に失敗しました。"));
+	MessageProc::AddEng(CMDFRK_SVCFAILEDTOSTART, _T("CmdFreak service initiation failed."));
+
+	//3143 (NC6)
+	MessageProc::AddJpn(CMDFRK_SVCFAILEDTOSTOP, _T("CmdFreakサービスの停止に失敗しました。"));
+	MessageProc::AddEng(CMDFRK_SVCFAILEDTOSTOP, _T("CmdFreak service termination failed."));
+
+	//3144 (NC6)
+	MessageProc::AddJpn(CMDFRK_SVCALREADYSTART, _T("CmdFreakサービスはすでに開始しています。"));
+	MessageProc::AddEng(CMDFRK_SVCALREADYSTART, _T("CmdFreak service has already been started."));
+
+	//3145 (NC6)
+	MessageProc::AddJpn(CMDFRK_SVCALREADYSTOP, _T("CmdFreakサービスはすでに停止しています。"));
+	MessageProc::AddEng(CMDFRK_SVCALREADYSTOP, _T("CmdFreak service has already been stopped."));
+
+	//3146 (NC6)
+	MessageProc::AddJpn(CMDFRK_SVCSTART, _T("CmdFreakサービスを開始しました。"));
+	MessageProc::AddEng(CMDFRK_SVCSTART, _T("CmdFreak service has started."));
+
+	//3147 (NC6)
+	MessageProc::AddJpn(CMDFRK_SVCSTOP, _T("CmdFreakサービスを停止しました。"));
+	MessageProc::AddEng(CMDFRK_SVCSTOP, _T("CmdFreak service has stopped."));
+
 
 	ZeroMemory(&si,sizeof(si));
 	si.cb=sizeof(si);
@@ -38,7 +69,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
 	if (strstr(Param, "start") != 0) {
 		if (IsProcessStarting() == TRUE) {
-			MessageBox(NULL, MessageProc::GetMsg(MessageProc::CMDFRK_SVCALREADYSTART), MessageProc::GetMsg(MessageProc::CMDFRK_SVCALREADYSTART), MB_OK);
+			MessageBox(NULL, MessageProc::GetMsg(CMDFRK_SVCALREADYSTART), MessageProc::GetMsg(CMDFRK_SVCALREADYSTART), MB_OK);
 			return 0;
 		}
 		wsprintf(CmdLineForNet, _T("\"%s\\net.exe\" start CMDFRKSRV"), SystemDir);
@@ -46,15 +77,15 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 		WaitForSingleObject(pi.hProcess, INFINITE);
 		CloseHandle(pi.hProcess);
 		if (IsProcessStarting() == TRUE) {
-			MessageBox(NULL, MessageProc::GetMsg(MessageProc::CMDFRK_SVCSTART), MessageProc::GetMsg(MessageProc::CMDFRK_SVCSTART), MB_OK);
+			MessageBox(NULL, MessageProc::GetMsg(CMDFRK_SVCSTART), MessageProc::GetMsg(CMDFRK_SVCSTART), MB_OK);
 		} else {
-			MessageBox(NULL, MessageProc::GetMsg(MessageProc::CMDFRK_SVCFAILEDTOSTART), MessageProc::GetMsg(MessageProc::CMDFRK_SVCFAILEDTOSTART), MB_OK);
+			MessageBox(NULL, MessageProc::GetMsg(CMDFRK_SVCFAILEDTOSTART), MessageProc::GetMsg(CMDFRK_SVCFAILEDTOSTART), MB_OK);
 		}
 	}
 
 	if (strstr(Param, "stop") != 0) {
 		if (IsProcessStarting() == FALSE) {
-			MessageBox(NULL, MessageProc::GetMsg(MessageProc::CMDFRK_SVCALREADYSTOP), MessageProc::GetMsg(MessageProc::CMDFRK_SVCALREADYSTOP), MB_OK);
+			MessageBox(NULL, MessageProc::GetMsg(CMDFRK_SVCALREADYSTOP), MessageProc::GetMsg(CMDFRK_SVCALREADYSTOP), MB_OK);
 			return 0;
 		}
 		wsprintf(CmdLineForNet, _T("\"%s\\net.exe\" stop CMDFRKSRV"), SystemDir);
@@ -62,9 +93,9 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 		WaitForSingleObject(pi.hProcess, INFINITE);
 		CloseHandle(pi.hProcess);
 		if (IsProcessStarting() == FALSE) {
-			MessageBox(NULL, MessageProc::GetMsg(MessageProc::CMDFRK_SVCSTOP), MessageProc::GetMsg(MessageProc::CMDFRK_SVCSTOP), MB_OK);
+			MessageBox(NULL, MessageProc::GetMsg(CMDFRK_SVCSTOP), MessageProc::GetMsg(CMDFRK_SVCSTOP), MB_OK);
 		} else {
-			MessageBox(NULL, MessageProc::GetMsg(MessageProc::CMDFRK_SVCFAILEDTOSTOP), MessageProc::GetMsg(MessageProc::CMDFRK_SVCFAILEDTOSTOP), MB_OK);
+			MessageBox(NULL, MessageProc::GetMsg(CMDFRK_SVCFAILEDTOSTOP), MessageProc::GetMsg(CMDFRK_SVCFAILEDTOSTOP), MB_OK);
 		}
 	}
 
