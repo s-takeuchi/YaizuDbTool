aa7a0f1b917b29f508b789f36f43779964331fc4
Shinya Takeuchi <bamboo@rj.dream.jp>
2015-09-06 13:01:44 +0900

    #3 For bbb

---
+++ src/bbb/MyMsgProc.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class MyMsgProc
+{
+public:
+
+	static void AddMsg();
+	static TCHAR* GetMsg(int);
+	static BYTE* GetMsgSjis(int);
+	static void StkErr(int, HWND);
+	static void StkErr(int, TCHAR*, HWND);
+	static void StkInf(int, HWND);
+	static void StkInf(int, TCHAR*, HWND);
+	static int StkYesNo(int, HWND);
+	static int StkYesNo(int, TCHAR*, HWND);
+};
