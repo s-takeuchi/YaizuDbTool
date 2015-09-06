802ed40a024f2895d1e9e9b46fe3a5f66dce1b99
Shinya Takeuchi <bamboo@rj.dream.jp>
2015-09-06 10:44:22 +0900

    #3 For cmdfrksrv

---
+++ src/cmdfrksrv/MyMsgProc.cpp
@@ -0,0 +1,43 @@
+#include "..\..\..\YaizuComLib\src\\msgproc\msgproc.h"
+#include "MyMsgProc.h"
+
+void MyMsgProc::AddMsg()
+{
+	// =====================================================================================================
+
+}
+
+TCHAR* MyMsgProc::GetMsg(int Id)
+{
+	return MessageProc::GetMsg(Id);
+}
+
+void MyMsgProc::StkErr(int Id, HWND WndHndl)
+{
+	MessageProc::StkErr(Id, WndHndl);
+}
+
+void MyMsgProc::StkErr(int Id, TCHAR* Str, HWND WndHndl)
+{
+	MessageProc::StkErr(Id, Str, WndHndl);
+}
+
+void MyMsgProc::StkInf(int Id, HWND WndHndl)
+{
+	MessageProc::StkInf(Id, WndHndl);
+}
+
+void MyMsgProc::StkInf(int Id, TCHAR* Str, HWND WndHndl)
+{
+	MessageProc::StkInf(Id, Str, WndHndl);
+}
+
+int MyMsgProc::StkYesNo(int Id, HWND WndHndl)
+{
+	return MessageProc::StkYesNo(Id, WndHndl);
+}
+
+int MyMsgProc::StkYesNo(int Id, TCHAR* Str, HWND WndHndl)
+{
+	return MessageProc::StkYesNo(Id, Str, WndHndl);
+}
