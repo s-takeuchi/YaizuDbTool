ab886186b0e55c45424abbaaba5a161453180311
Shinya Takeuchi <bamboo@rj.dream.jp>
2017-08-20 11:01:14 +0900

    #15: API regarding odbcinfos has been updated and UI has been enhanced.

--- src/restapi/ApiOdbcInfos.cpp
+++ src/restapi/ApiOdbcInfos.cpp
@@ -9,30 +9,43 @@ StkObject* ApiOdbcInfos::Execute(StkObject* ReqObj, int Method, TCHAR UrlPath[12
 {
 	SQLTCHAR ConnStrPostgreSql[Global::MAX_PARAM_LENGTH];
 	SQLTCHAR ConnStrMariaDb[Global::MAX_PARAM_LENGTH];
+	SQLTCHAR ConnStrMySql[Global::MAX_PARAM_LENGTH];
 
 	DbAccessor* DaPostgreSql = NULL;
 	DbAccessor* DaMariaDb    = NULL;
+	DbAccessor* DaMySql      = NULL;
 
 	DaPostgreSql = OdbcManager::GetInstance()->CreateAccessorObject(OdbcManager::POSTGRESQL_ACCESSOR);
-	DaMariaDb = OdbcManager::GetInstance()->CreateAccessorObject(OdbcManager::MARIADB_ACCESSOR);
+	DaMariaDb    = OdbcManager::GetInstance()->CreateAccessorObject(OdbcManager::MARIADB_ACCESSOR);
+	DaMySql      = OdbcManager::GetInstance()->CreateAccessorObject(OdbcManager::MYSQL_ACCESSOR);
 
 	DaPostgreSql->GetDefaultConnStr(ConnStrPostgreSql);
 	DaMariaDb->GetDefaultConnStr(ConnStrMariaDb);
+	DaMySql->GetDefaultConnStr(ConnStrMySql);
 
 	OdbcManager::GetInstance()->DeleteAccessorObject(DaPostgreSql);
 	OdbcManager::GetInstance()->DeleteAccessorObject(DaMariaDb);
+	OdbcManager::GetInstance()->DeleteAccessorObject(DaMySql);
 
 	StkObject* ResObj = new StkObject(_T(""));
 	AddCodeAndMsg(ResObj, 0, _T(""), _T(""));
 	StkObject* DatObj = new StkObject(_T("Data"));
+
 	StkObject* DatObjPostgreSql = new StkObject(_T("OdbcInfo"));
 	DatObjPostgreSql->AppendChildElement(new StkObject(_T("DbType"), _T("PostgreSQL")));
 	DatObjPostgreSql->AppendChildElement(new StkObject(_T("ConnStr"), ConnStrPostgreSql));
+	DatObj->AppendChildElement(DatObjPostgreSql);
+
 	StkObject* DatObjMariaDbSql = new StkObject(_T("OdbcInfo"));
 	DatObjMariaDbSql->AppendChildElement(new StkObject(_T("DbType"), _T("MariaDB")));
 	DatObjMariaDbSql->AppendChildElement(new StkObject(_T("ConnStr"), ConnStrMariaDb));
-	DatObj->AppendChildElement(DatObjPostgreSql);
 	DatObj->AppendChildElement(DatObjMariaDbSql);
+
+	StkObject* DatObjMySql = new StkObject(_T("OdbcInfo"));
+	DatObjMySql->AppendChildElement(new StkObject(_T("DbType"), _T("MySQL")));
+	DatObjMySql->AppendChildElement(new StkObject(_T("ConnStr"), ConnStrMySql));
+	DatObj->AppendChildElement(DatObjMySql);
+
 	ResObj->AppendChildElement(DatObj);
 
 	*ResultCode = 200;
