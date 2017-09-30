2686fa95e4efa07c9d548e98d66fa745654bdfd2
Shinya Takeuchi <bamboo@rj.dream.jp>
2017-09-30 09:27:22 +0900

    #18 : Skeleton of ApiTableInfo has been added.

---
+++ src/restapi/ApiTableInfo.cpp
@@ -0,0 +1,10 @@
+#include <tchar.h>
+#include "ApiTableInfo.h"
+#include "dataaccess.h"
+
+StkObject* ApiTableInfo::Execute(StkObject* ReqObj, int Method, TCHAR UrlPath[128], int* ResultCode, TCHAR Locale[3])
+{
+	*ResultCode = 200;
+
+	return NULL;
+}
