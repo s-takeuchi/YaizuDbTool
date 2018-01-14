1fc11cafed1509f5ab92cae045a7c9bf29e10664
Shinya Takeuchi <bamboo@rj.dream.jp>
2018-01-14 10:09:10 +0900

    #27 : bbb, cmdfrksrv, srvcmd and other related files have been deleted.

--- src/cmdfrksrv/WorkerThread.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <tchar.h>
-
-int StartWorkerThread();
-int StopWorkerThread();
--- src/cmdfrksrv/cmdfrksrv.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <windows.h>
-
-DWORD WINAPI HandlerEx(DWORD, DWORD, PVOID, PVOID);
-VOID WINAPI ServiceMain(DWORD dwArgc, PTSTR* pszArgv);
-void StartMongoose();
-void StopMongoose();
-BOOL CALLBACK EnumWindowsProc(HWND, LPARAM);
