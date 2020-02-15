a8496c5c66d3fde3acf365dd2132a1e229ab0a3c
Shinya Takeuchi <bamboo@rj.dream.jp>
2020-02-15 13:20:46 +0900

    #59 : Delete user API has been added.

---
+++ src/restapi/ApiDeleteUser.cpp
@@ -0,0 +1,10 @@
+#include "../../../YaizuComLib/src/stkpl/StkPl.h"
+#include "MyMsgProc.h"
+#include "ApiDeleteUser.h"
+#include "dataaccess.h"
+#include "..\Global.h"
+
+StkObject* ApiDeleteUser::ExecuteImpl(StkObject* ReqObj, int Method, wchar_t UrlPath[StkWebAppExec::URL_PATH_LENGTH], int* ResultCode, wchar_t Locale[3], wchar_t* Token)
+{
+	return NULL;
+}
