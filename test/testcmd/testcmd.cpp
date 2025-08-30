754f08e8bdacc6f2970fe8f6b2d61e1f19fa078b
Shinya Takeuchi <sy-takeuchi@outlook.com>
2025-08-30 21:24:53 +0900

    #90 :  Showing command usage has been impremented.

--- test/testcmd/testcmd.cpp
+++ test/testcmd/testcmd.cpp
@@ -2,10 +2,16 @@
 
 int main(int argc, char *argv[])
 {
-	StkPlPrintf("Test Command Line Arguments\n");
-	StkPlPrintf("Number of arguments: %d\n", argc);
-	for (int i = 0; i < argc; i++) {
-		StkPlPrintf("Argument %d: %s\n", i, argv[i]);
+	if (argc != 3) {
+		wchar_t CmdName[64] = L"";
+		wchar_t* PathToCmd = StkPlCreateWideCharFromUtf8(argv[0]);
+		StkPlGetFileNameFromFullPath(PathToCmd, CmdName, 64);
+		delete[] PathToCmd;
+		StkPlWPrintf(L"Usage: %ls odbc_connection_string test_scenario\n", CmdName);
+		StkPlPrintf("test_scenario:\n");
+		StkPlPrintf("    GENERAL ... 3 tables, 8 columns and 150 records in each table.\n");
+		StkPlExit(0);
 	}
+	
 	return 0;
 }
