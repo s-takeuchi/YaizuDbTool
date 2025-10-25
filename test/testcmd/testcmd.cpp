eff44a7e44c4a766adeb964fa1c142991ab3db18
Shinya Takeuchi <sy-takeuchi@outlook.com>
2025-10-25 13:05:56 +0900

    #91 : test program update

--- test/testcmd/testcmd.cpp
+++ test/testcmd/testcmd.cpp
@@ -7,113 +7,12 @@
 #include "../../../YaizuComLib/src/stkpl/StkPl.h"
 #include "../../../YaizuComLib/src/stkdb/DbAccessor.h"
 #include "../../../YaizuComLib/src/stkdb/DbPostgreSqlAccessor.h"
+#include "../../../YaizuComLib/src/stkdb/DbMySqlAccessor.h"
+#include "../../../YaizuComLib/src/stkdb/DbMariaDbAccessor.h"
 
 #define POSTGRESQL 1
 #define MYSQL 2
 #define MARIADB 3
-
-SQLHENV  Henv;
-SQLHDBC  Hdbc;
-SQLHSTMT Hstmt;
-
-
-void ConvertMessage(wchar_t StateMsg[10], wchar_t Msg[1024], const char16_t SqlStateMsg[10], const char16_t SqlMsg[1024])
-{
-	StkPlConvUtf16ToWideChar(StateMsg, 10, SqlStateMsg);
-	StkPlConvUtf16ToWideChar(Msg, 1024, SqlMsg);
-}
-
-// Return 0: Success, -1:Error
-int OpenDatabase(wchar_t* ConnectStr, wchar_t StateMsg[10], wchar_t Msg[1024])
-{
-	SQLWCHAR CvtConnectStr[256];
-	StkPlConvWideCharToUtf16((char16_t*)CvtConnectStr, 256, ConnectStr);
-
-	SQLWCHAR CvtStateMsg[10];
-	SQLWCHAR CvtMsg[1024];
-	SQLINTEGER Native; // This will not be refered from anywhere
-	SQLSMALLINT ActualMsgLen; // This will not be refered from anywhere
-	StkPlLStrCpy(Msg, L"");
-	StkPlLStrCpy(StateMsg, L"");
-
-	// Alloc environment handle
-	if (SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &(Henv)) == SQL_ERROR) {
-		if (Henv != SQL_NULL_HENV) {
-			SQLGetDiagRecW(SQL_HANDLE_ENV, Henv, 1, CvtStateMsg, &Native, CvtMsg, 1024, &ActualMsgLen);
-		}
-		ConvertMessage(StateMsg, Msg, (char16_t*)CvtStateMsg, (char16_t*)CvtMsg);
-		return -1;
-	}
-	SQLSetEnvAttr(Henv, SQL_ATTR_ODBC_VERSION, reinterpret_cast<SQLPOINTER>(SQL_OV_ODBC3), 0);
-
-	// Alloc DB connection handle
-	if (SQLAllocHandle(SQL_HANDLE_DBC, Henv, &Hdbc) == SQL_ERROR) {
-		SQLGetDiagRecW(SQL_HANDLE_ENV, Henv, 1, CvtStateMsg, &Native, CvtMsg, 1024, &ActualMsgLen);
-		ConvertMessage(StateMsg, Msg, (char16_t*)CvtStateMsg, (char16_t*)CvtMsg);
-		return -1;
-	}
-
-	// SQLDriverConnect
-	SQLWCHAR ConnOut[255]; // This will not be refered from anywhere
-	SQLSMALLINT ConnOutLen; // This will not be refered from anywhere
-	SQLRETURN Ret = SQLDriverConnectW(Hdbc, NULL, CvtConnectStr, SQL_NTS, ConnOut, 255, &ConnOutLen, SQL_DRIVER_COMPLETE);
-	if (Ret == SQL_ERROR || Ret == SQL_SUCCESS_WITH_INFO) {
-		SQLGetDiagRecW(SQL_HANDLE_DBC, Hdbc, 1, CvtStateMsg, &Native, CvtMsg, 1024, &ActualMsgLen);
-		ConvertMessage(StateMsg, Msg, (char16_t*)CvtStateMsg, (char16_t*)CvtMsg);
-		return -1;
-	}
-
-	// Alloc statement handle 
-	if (SQLAllocHandle(SQL_HANDLE_STMT, Hdbc, &Hstmt) == SQL_ERROR) {
-		SQLGetDiagRecW(SQL_HANDLE_DBC, Hdbc, 1, CvtStateMsg, &Native, CvtMsg, 1024, &ActualMsgLen);
-		ConvertMessage(StateMsg, Msg, (char16_t*)CvtStateMsg, (char16_t*)CvtMsg);
-		return -1;
-	}
-
-	return 0;
-}
-
-// Return 0: Success, -1:Error
-int CloseDatabase(wchar_t StateMsg[10], wchar_t Msg[1024])
-{
-	SQLWCHAR CvtStateMsg[10];
-	SQLWCHAR CvtMsg[1024];
-	SQLINTEGER Native; // This will not be refered from anywhere
-	SQLSMALLINT ActualMsgLen; // This will not be refered from anywhere
-	StkPlLStrCpy((wchar_t*)Msg, L"");
-	StkPlLStrCpy((wchar_t*)StateMsg, L"");
-
-	// Free statement handle
-	if (SQLFreeHandle(SQL_HANDLE_STMT, Hstmt) == SQL_ERROR) {
-		SQLGetDiagRecW(SQL_HANDLE_DBC, Hdbc, 1, CvtStateMsg, &Native, CvtMsg, 1024, &ActualMsgLen);
-		ConvertMessage(StateMsg, Msg, (char16_t*)CvtStateMsg, (char16_t*)CvtMsg);
-		return -1;
-	}
-
-	// SQLDisconnect
-	SQLRETURN Ret = SQLDisconnect(Hdbc);
-	if (Ret == SQL_ERROR || Ret == SQL_SUCCESS_WITH_INFO) {
-		SQLGetDiagRecW(SQL_HANDLE_DBC, Hdbc, 1, CvtStateMsg, &Native, CvtMsg, 1024, &ActualMsgLen);
-		ConvertMessage(StateMsg, Msg, (char16_t*)CvtStateMsg, (char16_t*)CvtMsg);
-		return -1;
-	}
-
-	// Free DB connection handle
-	if (SQLFreeHandle(SQL_HANDLE_DBC, Hdbc) == SQL_ERROR) {
-		SQLGetDiagRecW(SQL_HANDLE_ENV, Henv, 1, CvtStateMsg, &Native, CvtMsg, 1024, &ActualMsgLen);
-		ConvertMessage(StateMsg, Msg, (char16_t*)CvtStateMsg, (char16_t*)CvtMsg);
-		return -1;
-	}
-
-	// Free environment handle
-	if (SQLFreeHandle(SQL_HANDLE_ENV, Henv) == SQL_ERROR) {
-		SQLGetDiagRecW(SQL_HANDLE_ENV, Henv, 1, CvtStateMsg, &Native, CvtMsg, 1024, &ActualMsgLen);
-		ConvertMessage(StateMsg, Msg, (char16_t*)CvtStateMsg, (char16_t*)CvtMsg);
-		return -1;
-	}
-
-	return 0;
-}
 
 void ShowErrorMsg(wchar_t StateMsg[10], wchar_t Msg[1024])
 {
@@ -127,12 +26,26 @@ void ShowErrorMsg(wchar_t StateMsg[10], wchar_t Msg[1024])
 	StkPlPrintf("An error occurred!\n%s\n%s\n", ChStateMsg, ChMsg);
 }
 
-int TestGeneral(wchar_t* OdbcConStr)
+int TestGeneral(wchar_t* OdbcConStr, int DbmsType)
 {
 	int Ret = 0;
 	wchar_t StateMsg[10] = L"";
 	wchar_t Msg[1024] = L"";
-	DbAccessor* DbAcc = new DbPostgreSqlAccessor(OdbcConStr);
+	DbAccessor* DbAcc = NULL;
+	switch (DbmsType) {
+	case POSTGRESQL:
+		DbAcc = new DbPostgreSqlAccessor(OdbcConStr);
+		break;
+	case MYSQL:
+		DbAcc = new DbMySqlAccessor(OdbcConStr);
+		break;
+	case MARIADB:
+		DbAcc = new DbMariaDbAccessor(OdbcConStr);
+		break;
+	default:
+		break;
+	}
+
 	Ret = DbAcc->Test(StateMsg, Msg);
 	if (Ret != 0) {
 		ShowErrorMsg(StateMsg, Msg);
@@ -181,7 +94,7 @@ int main(int argc, char *argv[])
 		DbmsType = MARIADB;
 	}
 
-	if (TestGeneral(OdbcConStr) != 0) {
+	if (TestGeneral(OdbcConStr, DbmsType) != 0) {
 		//
 	}
 
